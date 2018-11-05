c52e74040757a4a3584c93ab12f119311038b606
Sergi Rene <s.rene@ucl.ac.uk>
2018-11-05 17:53:00 +0000

    video list

--- testbed/console/main.js
+++ testbed/console/main.js
@@ -7,46 +7,28 @@ var storageRef = firebase.storage().ref();
 const settings = {/* your settings... */ timestampsInSnapshots: true}; 
 db.settings(settings); 
 var currentUID;
+var file;
+var metadata;
 
 var videoListSection = document.getElementById('video-list');
 var deviceListSection = document.getElementById('device-list');
 var reportListSection = document.getElementById('report-list');
-
+var upButton = document.getElementById('upload-button'); 
 var videoFile = document.getElementById('file');
 
-
 var backendHostUrl = 'http://localhost:8080';
-
-var userTemplate;
 
 function handleFileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
-    var file = evt.target.files[0];
-    
-    var metadata = {
+    file = evt.target.files[0];
+    upButton.disabled = false;
+    metadata = {
         'contentType': file.type
     };
     
-    // Push to child path.
-    // [START oncomplete]
-    storageRef.child('images/' + file.name).put(file, metadata).then(function(snapshot) {
-    console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-    console.log('File metadata:', snapshot.metadata);
-                                                                     
-                                                                     // Let's get a download URL for the file.
-    snapshot.ref.getDownloadURL().then(function(url) {
-               console.log('File available at', url);
-             document.getElementById('linkbox').innerHTML = '<a href="' +  url + '">Click For File</a>';
-                                       
-        });
-    }).catch(function(error) {
-                                                                              // [START onfailure]
-                                                                              console.error('Upload failed:', error);
-                                                                              // [END onfailure]
-                                                                              });
-    // [END oncomplete]
 }
+
 /**
  * Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
  */
@@ -68,10 +50,111 @@ function onAuthStateChanged(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
+    listVideos(user);
+    listDevices(user);
+    upButton.addEventListener('click', function(){upload(user);}, false);	
+  } else {
+    // Set currentUID to null.
+    currentUID = null;
+    // Display the splash page where you can sign-in.
+    //splashPage.style.display = '';
+    window.location="/testbed";
+  }
+}
 
-	db.collection("users").get()
+function getFormattedDate(){
+    var d = new Date();
+
+    d = d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2);
+
+    return d;
+}
+
+function upload(user){
+    // Push to child path.
+    // [START oncomplete]
+    console.log("Upload");
+    upButton.disabled = true;
+    upButton.innerText = "Uploading...";
+    storageRef.child('testbed/' + file.name).put(file, metadata).then(function(snapshot) {
+    	console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+    	console.log('File metadata:', snapshot.metadata);
+        upButton.disabled = false;
+    	upButton.innerText = "Upload";                                  
+   	// Let's get a download URL for the file.
+    	snapshot.ref.getDownloadURL().then(function(url) {
+	var title = document.getElementById('title').value;
+	var desc = document.getElementById('desc').value;
+    	console.log('File available at', url,title,desc);
+	var video = {
+      		title: title,
+      		desc: desc,
+      		url: url,
+		user: user.uid,
+		time: getFormattedDate()
+    	}
+	db.collection("videos").add(video);	
+	});
+    }).catch(function(error) {
+     // [START onfailure]
+      console.error('Upload failed:', error);
+      // [END onfailure]
+    });
+    // [END oncomplete]
+}
+
+function listVideos(user) {
+
+   db.collection("videos").where("user", "==", user.uid)
+   //db.collection("videos").get()
+	 .onSnapshot(function(querySnapshot) {
+
+	    querySnapshot.forEach(function(doc) {
+		console.log(doc.data());
+		var node = document.createElement('dt');
+		var textnode = document.createTextNode(doc.data().title);         // Create a text node
+		node.appendChild(textnode); 	
+		document.getElementById("videos").appendChild(node);
+		node.addEventListener("click", function(){
+  			$('.dd--active').not($(this).next()).slideUp(300).removeClass('dd--active');
+  			$(this).next().toggleClass('dd--active').slideToggle(300)
+		});
+		node.addEventListener("click",function(t) {
+  			return "undefined" != typeof w && w.event.triggered !== t.type ? w.event.dispatch.apply(e, arguments) : void 0
+		});
+		var desc = document.createElement('dd');
+		
+		var name = document.createElement('span');
+		var textdesc = document.createTextNode(doc.data().desc);         // Create a text node
+		name.appendChild(textdesc); 
+		name.appendChild(document.createElement('br')); 
+		desc.appendChild(name);	
+		
+		var link = document.createElement('span');
+		var a = document.createElement('a');
+                a.setAttribute('href', doc.data().url);
+		a.appendChild(document.createTextNode("Video link" + ' '));
+		link.appendChild(a); 
+		link.appendChild(document.createElement('br')); 
+		desc.appendChild(link);	
+		
+		var time = document.createElement('span');
+		var textdesc = document.createTextNode("Uploaded time: "+doc.data().time);         // Create a text node
+		time.appendChild(textdesc); 
+		time.appendChild(document.createElement('br')); 
+		desc.appendChild(time);		
+		
+		document.getElementById("videos").appendChild(desc);
+
+	    });
+	});
+}
+
+function listDevices(user)
+{
+db.collection("users").get()
 	    .then(function(querySnapshot) {
-	    this.userTemplate = '<dl class="list list--numbered">';
+
 	    querySnapshot.forEach(function(doc) {
 		var node = document.createElement('dt');
 		var textnode = document.createTextNode(doc.data().model);         // Create a text node
@@ -105,13 +188,6 @@ function onAuthStateChanged(user) {
 	    });
 	});
 
-  } else {
-    // Set currentUID to null.
-    currentUID = null;
-    // Display the splash page where you can sign-in.
-    //splashPage.style.display = '';
-    window.location="/testbed";
-  }
 }
 /**
  * Handles the sign in button press.
@@ -158,7 +234,7 @@ window.addEventListener('load', function() {
   videoFile.addEventListener('change', handleFileSelect, false);
   videoFile.disabled = true;
 
-   showSection(deviceListSection);
+  showSection(videoListSection);
 
 }, false);
 
