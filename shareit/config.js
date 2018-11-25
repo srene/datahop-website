59372059db9a920077d99803088ba8e4a8301d76
alextsak <alextsak93@gmail.com>
2018-11-26 00:26:06 +0200

    new desktop app

--- shareit/config.js
+++ shareit/config.js
@@ -1,6 +1,6 @@
 let config = {};
-
-config.default_dir = "D:/shareit_data/";
+const homedir = require('os').homedir();
+config.default_dir = homedir + "/shareit_data/";
 config.db_url = "";
 config.db_port = "";
 module.exports = config;
